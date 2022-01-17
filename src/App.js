6d39e4edf9b961619e88974ae1b667a03b601ebe
huugo26 <kari.seuranen@gmail.com>
2022-01-17 14:44:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>Heippa kaikki nörtit</p>
+      <p>Mitäs kivaa tähän oikein kirjoittelisi? Skandimerkitkin näkyy hyvin. Ja tämä ohjelma näkyy osaavan rivinvaihdot jotenkin! Mitenkäs tyhjä rivi kirjoitetaan?</p>
+      <p>No ainakin uudella kappaleella</p>
+      <h2>Koe</h2>
+      <p> - </p>
+      <p> - </p>
+      <p>Uusi kappale....</p>
+    </div>
+  );
+}
